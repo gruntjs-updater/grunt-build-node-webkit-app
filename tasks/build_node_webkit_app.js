627b87a5236daef584d86a1d48242d841dbed2df
Jan-Henrik Bruhn <jh.bruhn@me.com>
2014-05-17 18:26:03 +0200

    Initial Commit.

---
+++ tasks/build_node_webkit_app.js
@@ -0,0 +1,50 @@
+/*
+ * grunt-build-node-webkit-app
+ * https://github.com/jhbruhn/grunt-build-node-webkit-app
+ *
+ * Copyright (c) 2014 Jan-Henrik Bruhn
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Please see the Grunt documentation for more information regarding task
+  // creation: http://gruntjs.com/creating-tasks
+
+  grunt.registerMultiTask('build_node_webkit_app', 'Another task for creation of Node-Webkit apps, uncompressed.', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" created.');
+    });
+  });
+
+};
